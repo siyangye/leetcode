eb28cbb3004d5c7ccae9726ba317d529c746eedb
siyangye <siyang.ye0825@gmail.com>
2023-02-09 21:35:44 -0800

    very first twosum...

---
+++ twosum.py
@@ -0,0 +1,14 @@
+#list all of the elements from the [num]
+def Twosum(nums,target):
+    #loop: pick the number from first to last
+    #len(num) 
+    for i in range(len(nums)):
+        #loop: pick the number from the 1st one following after:
+        for j in ((i+1), len(nums)):
+            sum = nums[i] + nums [j]
+            if sum == target:
+                return [i,j]
+
+
+result = Twosum([1,2,3], 3)
+print(result)

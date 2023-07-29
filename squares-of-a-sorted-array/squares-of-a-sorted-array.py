7f82f28ab6a03bef71651179b02224f7b9e23ecd
siyangye <50527023+siyangye@users.noreply.github.com>
2023-07-29 14:15:27 -0700

    Time: 137 ms, Memory: 15.6 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -20,54 +20,54 @@ class Solution(object):
         
         return sort[::-1] #reverse
              
-# #     #method 2: list it one by one 
-#         sort = []
-#         for num in nums:
-#             k = num **2 
-            
-#             i = 0
-#             if not sort or sort[-1] <= k:
-#                 sort.append(k)
-#             else:
-#                 sort.append(0) #append first
-#                 while i < len(sort) and sort[i] <= k:
-#                     i = i+1
-#                 #when: sort[i] >k, break the while loop 
-#                 # move the latter list forward, but you have to do it backwards, or it will get overwrite. 
-#                 j = len(sort) -1
-#                 while j > i:
-#                     sort[j] = sort[j-1]
-#                     j = j-1
-#                 sort[i] = k
-            
-
-#         return sort 
-        
-        sorted = []
-
+#     #method 2: list it one by one 
+        sort = []
         for num in nums:
-            #square it 
-            sqr = num ** 2 
+            k = num **2 
+            
+            i = 0
+            if not sort or sort[-1] <= k:
+                sort.append(k)
+            else:
+                sort.append(0) #append first
+                while i < len(sort) and sort[i] <= k:
+                    i = i+1
+                #when: sort[i] >k, break the while loop 
+                # move the latter list forward, but you have to do it backwards, or it will get overwrite. 
+                j = len(sort) -1
+                while j > i:
+                    sort[j] = sort[j-1]
+                    j = j-1
+                sort[i] = k
+            
 
-            # Find the proper position to place the new square
-            i = len(sorted);
-            while i > 0 and sorted[i - 1] > sqr:
-                i = i - 1
+        return sort 
+        
+#         sorted = []
 
-            # Expand the container first!!!
-            sorted.append(0)
+#         for num in nums:
+#             #square it 
+#             sqr = num ** 2 
 
-            # For all those after the position, move one position
-            # afterward; and, moving should start from the end!!!
-            j = len(sorted) - 1
-            while j > i:
-                sorted[j] = sorted[j - 1]
-                j = j - 1
+#             # Find the proper position to place the new square
+#             i = len(sorted);
+#             while i > 0 and sorted[i - 1] > sqr:
+#                 i = i - 1
 
-            # Plase the new square in the proper position
-            sorted[i] = sqr
+#             # Expand the container first!!!
+#             sorted.append(0)
 
-        return sorted
+#             # For all those after the position, move one position
+#             # afterward; and, moving should start from the end!!!
+#             j = len(sorted) - 1
+#             while j > i:
+#                 sorted[j] = sorted[j - 1]
+#                 j = j - 1
+
+#             # Plase the new square in the proper position
+#             sorted[i] = sqr
+
+#         return sorted
 
                 
             

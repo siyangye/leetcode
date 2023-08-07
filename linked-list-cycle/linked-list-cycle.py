b8cba413c477044654c9cf66084093b6ee594b9e
siyangye <50527023+siyangye@users.noreply.github.com>
2023-08-06 19:01:17 -0700

    Time: 49 ms, Memory: 20.6 MB - LeetHub

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -9,7 +9,7 @@ class Solution:
         if not head or not head.next or not head.next.next: 
             return False
         slow, fast = head, head #don't set to equal 
-        while fast and fast.next:
+        while fast and fast.next and slow.next: #I do not need to check the fast.next, the 
             #give the fast and slow pointer 
             slow = slow.next
             fast = fast.next.next

9c3c488f8addc628e58314151d692ded370c5684
siyangye <50527023+siyangye@users.noreply.github.com>
2023-08-12 17:31:04 -0700

    Time: 159 ms, Memory: 20.1 MB - LeetHub

---
+++ diagonal-traverse/diagonal-traverse.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
+        num_rows, num_cols = len(matrix), len(matrix[0])
+        diagonals = [[] for _ in range(num_rows+ num_cols -1)]
+        #create a 2d list to stroe all elements:
+        for i in range(num_rows):
+            for j in range(num_cols):
+                #难点:
+                diagonals[i+j].append(matrix[i][j])
+        #convert the 2d list into diagonal sequence
+        res = diagonals[0] #store the first row 
+        for p in range(1,len(diagonals)): #reverse the rest of the rows
+            if p % 2 ==0:
+                #reverse even rows:
+                res.extend(diagonals[p][::-1]) #extend element, append whole list
+            else:
+                res.extend(diagonals[p])
+        #return out of the loop
+        return res
+            
+        

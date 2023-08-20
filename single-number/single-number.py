efbab25701bf2f79e586cf47d900f9d776cf386f
siyangye <50527023+siyangye@users.noreply.github.com>
2023-08-20 16:28:41 -0700

    Time: 114 ms, Memory: 19.4 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,13 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        n = 0 
-        while n+1 < len(nums):
-            if nums[n] == nums[n+1]:
-                # del nums[n]
-                # del nums[n+1]
-                n += 2 
-            else:
-                return nums[n]
-        return nums[n]
+        # nums.sort()
+        # n = 0 
+        # while n+1 < len(nums):
+        #     if nums[n] == nums[n+1]:
+        #         # del nums[n]
+        #         # del nums[n+1]
+        #         n += 2 
+        #         #don't delete element while iterating the length of the array 
+        #     else:
+        #         return nums[n]
+        # return nums[n]
+        
+        hashSet = set()
+        for n in nums:
+            if n in hashSet:
+                hashSet.remove(n)
+            else: 
+                hashSet.add(n)
+        return hashSet.pop()
+            
         

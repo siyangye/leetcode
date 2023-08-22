8088fac621f20fdae1f3498c881b2f8e1e7c8f3b
siyangye <50527023+siyangye@users.noreply.github.com>
2023-08-22 15:44:48 -0700

    Time: 107 ms, Memory: 16.3 MB - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -4,14 +4,14 @@ class Solution:
         rows = collections.defaultdict(set)
         squares = collections.defaultdict(set)
         
-        #check existence:
-                
-        #check hashMaps: 
+
+        #check existence: 
         for i in range(9):
             for j in range(9):
                 if board[i][j] == ".":
-                    continue
-
+                    continue 
+                
+                #check hashMap: 
                 elif (board[i][j] in rows[i] or 
                 board[i][j] in cols[j] or
                 board[i][j] in squares[(i//3, j//3)]):

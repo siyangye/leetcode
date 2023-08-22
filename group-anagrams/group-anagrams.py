79ceb676916329b647c5d5263cdb341865a81e79
siyangye <50527023+siyangye@users.noreply.github.com>
2023-08-21 23:26:46 -0700

    Time: 101 ms, Memory: 19.4 MB - LeetHub

---
+++ group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #key: sorted words "tea" -> ['a','e','t']
+        #value: corresponding word 
+        #{key:value, key:value}, return the value 
+        hashMap = {}
+        for i in strs:
+            #key = sorted(i) #this is a list, you cannot use list as key as it is mutable
+            key = ''.join(sorted(i))
+            print(key)
+            if key in hashMap:
+                hashMap[key].append(i)
+            else:
+                hashMap[key] = [i] 
+        anagramList = list(hashMap.values())
+        return anagramList 

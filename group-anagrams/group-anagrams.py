c8d9d332e4eaa7961eb0c165ed5da7139717c66e
siyangye <50527023+siyangye@users.noreply.github.com>
2023-08-21 23:35:53 -0700

    Time: 103 ms, Memory: 19.4 MB - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -3,6 +3,7 @@ class Solution:
         #key: sorted words "tea" -> ['a','e','t']
         #value: corresponding word 
         #{key:value, key:value}, return the value 
+        #correct: 
         hashMap = {}
         for i in strs:
             #key = sorted(i) #this is a list, you cannot use list as key as it is mutable
@@ -14,3 +15,19 @@ class Solution:
                 hashMap[key] = [i] 
         anagramList = list(hashMap.values())
         return anagramList 
+        
+        # #有问题的code:
+        #    #key: sorted words "tea" -> ['a','e','t']
+        # #value: corresponding word 
+        # #{key:value, key:value}, return the value 
+        # hashMap = {}
+        # for i in strs:
+        #     #key = sorted(i) #this is a list, you cannot use list as key as it is mutable
+        #     key = ''.join(sorted(i))
+        #     print(key)
+        #     if key in hashMap:
+        #         hashMap[key].append(i)
+        #     hashMap[key] = [i] 
+        # anagramList = list(hashMap.values())
+        # return anagramList 
+    

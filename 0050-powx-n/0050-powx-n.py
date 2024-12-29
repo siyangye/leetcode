8d98faf8d46706873a98ee39105e55bce634cb9f
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2024-12-29 15:02:30 -0800

    Time: 0 ms (100%), Space: 18.1 MB (7%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,26 +1,27 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if x == 0:
-            return 0
-        elif n == 0:
-            return 1
-        # elif n == 1:
-        #     return x 
-        elif n >= 1:
-            res = 1 
-            while n >=1:
-                res *= x
-                n -= 1 
-            return res
-        # elif n == -1:
-        #     return 1/x
-        elif n <= -1:
-            power = -1 *n
-            res = 1
-            while power >=1:
-                res *= x
-                power-= 1
-            return 1/res 
+        # if x == 0:
+        #     return 0
+        # elif n == 0:
+        #     return 1
+        # # elif n == 1:
+        # #     return x 
+        # elif n >= 1:
+        #     res = 1 
+        #     while n >=1:
+        #         res *= x
+        #         n -= 1 
+        #     return res
+        # # elif n == -1:
+        # #     return 1/x
+        # elif n <= -1:
+        #     power = -1 *n
+        #     res = 1
+        #     while power >=1:
+        #         res *= x
+        #         power-= 1
+        #     return 1/res 
+        return pow(x,n)
 
 
 

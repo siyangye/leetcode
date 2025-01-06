02c79eb9c0b2670a5f3a6ab9336be883df0ba1fc
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-05 16:28:23 -0800

    Time: 7 ms (54.64%), Space: 20.8 MB (25.77%) - LeetHub

---
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -0,0 +1,20 @@
+class MovingAverage:
+    def __init__(self, size: int):
+        self.size = size
+        self.list =[]
+        self.window_sum =0
+
+    def next(self, val: int) -> float:
+        self.list.append(val)
+        self.window_sum+= val 
+        if len(self.list)> self.size:
+            self.window_sum-=self.list.pop(0)
+        return self.window_sum/len(self.list)
+        
+        
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)

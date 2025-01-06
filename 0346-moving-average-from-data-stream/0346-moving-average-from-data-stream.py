7dd753cb9b4781d633f493c42f1cee4a43bc3668
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-05 16:34:33 -0800

    Time: 11 ms (36.25%), Space: 21 MB (22.95%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,18 +1,17 @@
-#use list: runtime:O(n)cuz each list.pop(0)require moving rest of the elements in the list; space compelxity:O(size) 
+#use deque :runtime complexity: O(1)
 class MovingAverage:
+
     def __init__(self, size: int):
         self.size = size
-        self.list =[]
-        self.window_sum =0
+        self.list = deque()
+        self.sums = 0
 
     def next(self, val: int) -> float:
         self.list.append(val)
-        self.window_sum+= val 
+        self.sums += val
         if len(self.list)> self.size:
-            self.window_sum-=self.list.pop(0)
-        return self.window_sum/len(self.list)
-        
-        
+            self.sums-=self.list.popleft()
+        return self.sums/len(self.list)
         
 
 

f3e3ef07c39babd08ecfc3848dccd597ef6c78f6
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-06 16:39:37 -0800

    Time: 255 ms (63.5%), Space: 91.4 MB (42.69%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -1,31 +1,13 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
         @cache
-        def helper(left: int, right: int, k: int) -> bool:
-            # 基本情况
-            if k < 0:  # 删除次数用完
-                return False
-            if left >= right:  # 检查完所有字符
-                return True
-                
-            # 如果字符相同，继续检查内部字符
-            if s[left] == s[right]:
-                return helper(left + 1, right - 1, k)
-            
-            # 字符不同，尝试删除左边或右边的字符
-            return helper(left + 1, right, k - 1) or \
-                   helper(left, right - 1, k - 1)
+        def moves(l,r)-> int:
+            if l >= r:
+                return 0
+            elif s[l]==s[r]:
+                return moves(l+1,r-1)
+            else:
+                return 1+ min(moves(l+1,r),moves(l,r-1))
+        return moves(0,len(s)-1)<=k
+
         
-        return helper(0, len(s) - 1, k)
-        # @cache  # 添加记忆化
-        # def minDeletions(left: int, right: int) -> int:
-        #     if left >= right:
-        #         return 0
-                
-        #     if s[left] == s[right]:
-        #         return minDeletions(left + 1, right - 1)
-            
-        #     return 1 + min(minDeletions(left + 1, right), 
-        #                  minDeletions(left, right - 1))
-        
-        # return minDeletions(0, len(s) - 1) <= k

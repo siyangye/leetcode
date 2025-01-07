6b2265beb1d52772604235feeefa24431274967b
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-06 17:41:45 -0800

    Time: 11 ms (67.78%), Space: 18.2 MB (6.2%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,11 +1,26 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        res =[]
-        for i in range(len(boxes)):
-            curr_moves =0
-            for j in range(len(boxes)):
-                if boxes[j]== '1':
-                    curr_moves += abs(i-j)
-            res.append(curr_moves)
+        res = [0]*len(boxes)
+
+        count_ones =0
+        accumulated_moves = 0
+
+        for i in range(len(boxes)):#[0],c=1,acumu=1,[1],c=2,[3]
+            res[i]+= accumulated_moves
+            # print(accumulated_moves)
+            if boxes[i]=='1':
+                count_ones +=1
+            accumulated_moves += count_ones
+            # print(accumulated_moves)
+        print(res)
+        left_count_ones =0
+        left_accumulated_moves = 0
+        n=len(boxes)
+        for i in range(n-1,-1,-1):
+            res[i]+= left_accumulated_moves
+            if boxes[i]=='1':
+                left_count_ones +=1
+            left_accumulated_moves += left_count_ones
+                # print(left_accumulated_moves)
+        print(res)
         return res 
-        

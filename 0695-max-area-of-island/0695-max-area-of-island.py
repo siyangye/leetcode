c7fe8f56906515c36d82c875dc99f867f1e6a964
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-10 17:35:00 -0800

    Time: 30 ms (35.33%), Space: 18.4 MB (38.26%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        directions=[(0,-1),(0,1),(-1,0),(1,0)]
+        def dfs(r,c)->int:
+            if 0<=r<len(grid) and 0<=c<len(grid[0])and grid[r][c]==1:
+                grid[r][c]=0
+                area = 1
+                for inc_r,inc_c in directions:#loop
+                    area += dfs(inc_r+r,inc_c+c)
+            else:
+                return 0
+            return area
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                res = max(res,dfs(i,j))
+        return res
+
+        

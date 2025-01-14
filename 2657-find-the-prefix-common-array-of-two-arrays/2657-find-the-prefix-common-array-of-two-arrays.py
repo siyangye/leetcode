d3562b1fe6698a8973e9120d3ac6de5c321ad481
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-14 13:29:41 -0800

    Time: 4 ms (85.86%), Space: 17.9 MB (20.16%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -1,34 +1,27 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        if len(A)==1:
-            return [1]
-        ind_dict ={}
+        seen = set()
+        result = []
+        count = 0
+        
         for i in range(len(A)):
-            # print(i)
-            if A[i] not in ind_dict:
-                ind_dict[A[i]]=i
-                print(i)
-            if A[i] in ind_dict:
-                curr_ind = ind_dict[A[i]]
-                if i > curr_ind:
-                    ind_dict[A[i]]=i
-            if B[i] not in ind_dict:
-                ind_dict[B[i]]=i
-            if B[i] in ind_dict:
-                curr_ind = ind_dict[B[i]]
-                if i > curr_ind:
-                    ind_dict[B[i]]=i 
-        count = {}
-        for c,ind in ind_dict.items():
-            # print(c)
-            count[ind]=count.get(ind,0)+1
-        res = []
-        curr=0
-        for j in range(len(A)):
-            if j in count:
-                curr+= count[j]
-            res.append(curr)
-        return res
+            # Add current elements to seen set
+            curr_a= A[i]
+            curr_b=B[i]
             
+            # If same element, count it once
+            if A[i] == B[i]:
+                count += 1
+            # If different elements, check if we've seen them before
+            else:
+                # Check if B[i] exists in previous A elements
+                if B[i] in seen:
+                    count += 1
+                # Check if A[i] exists in previous B elements
+                if A[i] in seen:
+                    count += 1
+            seen.add(curr_a)
+            seen.add(curr_b)
+            result.append(count)
         
-        
+        return result

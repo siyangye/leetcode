25d9f4d13e1af3b6585573f0ac735f1c1668dd3f
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-13 16:32:12 -0800

    Time: 4 ms (72.24%), Space: 21.2 MB (9.31%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -3,14 +3,16 @@ class MovingAverage:
     def __init__(self, size: int):
         self.queue=deque()
         self.size = size
+        self.sum = 0
 
     def next(self, val: int) -> float:
         self.queue.append(val)
+        self.sum+=val
         if len(self.queue)<=self.size:
-            return sum(self.queue)/len(self.queue)
+            return self.sum/len(self.queue)
         else:
-            self.queue.popleft()
-            return sum(self.queue)/len(self.queue)
+            self.sum-=self.queue.popleft()
+            return self.sum/len(self.queue)
         
 
 

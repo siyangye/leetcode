d091a22ed8588c54223fcec854dcb5a7e489a275
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-25 21:09:39 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        def is_valid_int(s:str):
+            seen_num = False
+            if not s:
+                return False
+            i=0
+            if s[i] in ['+','-']:
+                i+=1
+            while i<len(s):
+                if s[i].isdigit():
+                    i+=1
+                    seen_num = True
+                else:
+                    return False
+            return seen_num
+
+        seen_num = False
+        count_dot = 0
+        count_e = 0
+        i=0
+        if s[i] in ['+','-']:
+            i+= 1
+        while i < len(s):
+            if s[i]=='.':
+                count_dot += 1
+            elif s[i] in ['e','E']:
+                count_e +=1
+                return seen_num and is_valid_int(s[i+1:])
+            elif s[i].isdigit():
+                seen_num = True
+            else:
+                return False
+            i+=1
+        return count_dot <=1 and count_e <=1 and seen_num

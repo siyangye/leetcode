5b27cf8413c8de02b757021a956545387a729f4f
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-01-31 14:54:56 -0800

    Time: 3 ms (50.21%), Space: 19.6 MB (78.93%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,38 +1,26 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        n = len(intervals)
         i = 0
-        if not intervals:
-            res.append(newInterval)
-            return res
-        if intervals[-1][1]<newInterval[0]:
-            intervals.append(newInterval)
-            return intervals
-        if intervals[0][0]>newInterval[1]:
-            res.append(newInterval)
-            while i<n:
-                res.append(intervals[i])
-                i+=1
-            return res
-
-        while i < n and intervals[i][1]<newInterval[0]:
-            res.append(intervals[i])
-            i+=1
-        #what if i==n??
-        if i==n:
-            res.append(newInterval)
-            return res
-        merge = [min(intervals[i][0],newInterval[0]),min(intervals[i][1],newInterval[1])]
-        while i < n and intervals[i][0]<=newInterval[1]:
-            merge[1]=max(merge[1],intervals[i][1])
-            i+=1
-        res.append(merge)
-        while i < n: #intervals[i][0]> newIntervals[1]
-            res.append(intervals[i])
-            i+=1
-        return res
+        n = len(intervals)
         
-            
-
-
+        # Add all intervals that come before the new interval
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        
+        # Merge overlapping intervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        
+        # Add the merged interval
+        res.append(newInterval)
+        
+        # Add remaining intervals
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+        
+        return res

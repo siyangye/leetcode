3ed3b3a6278c6ca239943387d480fe7e718b4675
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-02-05 17:27:23 -0800

    Time: 0 ms (100%), Space: 18 MB (57.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,35 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if nums[0] <= nums[-1]:
-            l = 0
-            r = len(nums) - 1
-            while l <=r:
-                mid = (l+r) //2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    r = mid -1
-                else:
-                    l = mid + 1
-            return -1
-
-        else:
-            index = {}
-            for i in range(len(nums)):
-                index[nums[i]] = i
-                if i+1 < len(nums) and nums[i] > nums[i+1]:
-                    array = nums[i+1:] + nums[:i+1]
-            l = 0
-            r = len(array) - 1
-            while l <=r:
-                mid = (l + r) //2
-                if array[mid] == target:
-                    res = index[target]
-                    return res
-                elif array[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+        if not nums:
             return -1
             
-
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+                
+            # 判断左半部分是否有序
+            if nums[left] <= nums[mid]:
+                # 如果target在左半部分的范围内
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1  # 往左找
+                else:
+                    left = mid + 1   # 往右找
+            # 右半部分有序
+            else:
+                # 如果target在右半部分的范围内
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1   # 往右找
+                else:
+                    right = mid - 1  # 往左找
+        return -1

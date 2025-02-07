d110ac7cffedfd1c22043249c1ac95211c3da339
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-02-06 22:56:05 -0800

    Time: 121 ms (77.57%), Space: 20.3 MB (67.63%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        row = len(mat)
+        col = len(mat[0])
+        dist = [[float('inf')] * col for i in range(row)]
+        queue = deque([])
+        for i in range(row):
+            for j in range(col):
+                if mat[i][j] == 0:
+                    dist[i][j] = 0
+                    queue.append((i,j))
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        while queue:
+            r,c = queue.popleft()
+            for x,y in directions:
+                new_row = x + r
+                new_col = y + c
+                if 0 <= new_row < row and 0 <=new_col < col:
+                    if dist[new_row][new_col] > dist[r][c] + 1:
+                        dist[new_row][new_col] = dist[r][c] + 1
+                        queue.append((new_row, new_col))
+        return dist
+            
+        

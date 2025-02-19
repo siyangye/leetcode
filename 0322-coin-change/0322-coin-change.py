cf44d8ea342377978d4d03eec9d9e8f4a96ba4b3
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-02-18 19:27:43 -0800

    Time: 775 ms (52.21%), Space: 18.4 MB (43.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,27 +1,47 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # if amount == 0:
-        #     return 0
-        res = []
-        def dfs(i, path,curr_sum):
-            #base case:
-            if curr_sum == amount:
-                res.append(path)
-                return
-            if curr_sum > amount:
-                return
-            if i < len(coins):
-                return
-            #if not:
-            path.append(coins[i])
-            dfs(i,path,curr_sum + coins[i])
-            path.pop()
-            dfs(i+1,path,curr_sum)
-        dfs(0,[],0)
-        if not res:
-            return -1
-        min_amount = len(res[0])
-        for sums in res:
-            min_amount = min(min_amount, len(sums))
-        return min_amount
+        # Top Down DP (Memoization)
+        # Time: O(Coins * Amount)
+        # Space: O(Amount)
+        coins.sort()
+        memo = {0:0}
+        def min_coins(amt):
+            if amt in memo:
+                return memo[amt]
 
+            minn = float('inf')
+            for coin in coins:
+                diff = amt - coin
+                if diff < 0:
+                    break
+                minn = min(minn, 1 + min_coins(diff))
+
+            memo[amt] = minn
+            return minn
+        result = min_coins(amount)
+        if result < float('inf'):
+            return result
+        else:
+            return -1
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Bottom Up DP (Tabulation)
+        # Time: O(Coins * Amount)
+        # Space: O(Amount)
+        dp = [0] * (amount + 1)
+        coins.sort()
+        for i in range(1, amount+1):
+            minn = float('inf')
+
+            for coin in coins:
+                diff = i - coin
+                if diff < 0:
+                    break
+                minn = min(minn, dp[diff] + 1)
+
+            dp[i] = minn
+
+        if dp[amount] < float('inf'):
+            return dp[amount]
+        else:
+            return -1

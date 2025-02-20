f573e2a4fd58d099f794ec66d7444c70b76ba686
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-02-20 14:56:11 -0800

    Time: 0 ms (100%), Space: 18.6 MB (53.89%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -7,20 +7,14 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return
-        s = []
-        while head:
-            s.append(head)
-            head = head.next
-        #no head anymore
-        current = end = ListNode(0)
-        while s:
-            end.next = s.pop()
-            end = end.next
-            end.next = None #这个地方很容易错！这样才能打破旧的next pointer关系！
-        return current.next
+        curr = head
+        prev = None #what does that even mean? is it a listnode? 
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
 
 
-            
-            
-
-            
+        

667b5e0fffb2f7583a584561e311b08967b4a9c6
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-02-26 14:11:32 -0800

    Time: 15 ms (14.45%), Space: 24.1 MB (5.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l = 0
-        r = len(s) - 1
-        while l < r:
-            if not s[l].isalnum(): #if this is still not alnum, we should keep looping 
-                l += 1
-                continue #this will make sure we loop till l point to alnum. 
-            if not s[r].isalnum():
-                r -= 1
-                continue
-            if s[l].lower() == s[r].lower():
-                l += 1
-                r -= 1
-            else:
-                return False
-        return True
-            
+        string = []
+        for c in s:
+            if c.isalnum():
+                string.append(c.lower())
+        print(string)
+        return string == string[::-1] #reverse list 

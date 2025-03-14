4b80b0c90f53fa7be51c9a0577965608240237bb
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-03-14 16:54:29 -0700

    Time: 7 ms (73.35%), Space: 18.4 MB (14.61%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,30 +1,24 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        bed = [0]
-        for fb in flowerbed:
-            bed.append(fb)
-        bed.append(0)
-        # print(bed)
-        curr = 0
-        count = [] #zero
-        for i in range(len(bed)):
-            if bed[i] == 0:
-                curr += 1
-            else:
-                count.append(curr)
-                #reset:
-                curr = 0
-        count.append(curr)
-        max_count = 0
-        print(count)
-        for c in count:
-            if c >= 3:
-                c -= 2
-                if c % 2 == 0:
-                    max_count += c // 2
-                else:
-                    max_count += c// 2 + 1
-        print(max_count)
-        return max_count >= n
-
+        bed = [0] + flowerbed + [0]
+        
+        count = 0
+        consecutive_zeros = 0
+        
+        for plot in bed:
+            if plot == 0:
+                consecutive_zeros += 1
+            else:  # plot == 1
+                # Calculate how many flowers we can plant in this segment
+                if consecutive_zeros > 2:
+                    # We can place (z-1)//2 flowers in z consecutive zeros between flowers
+                    count += (consecutive_zeros - 1) // 2
+                    print(count)
+                consecutive_zeros = 0
+                
+        # Add the last segment if it ends with zeros
+        if consecutive_zeros > 2:
+            count += (consecutive_zeros -1) // 2
             
+        return count >= n
+        

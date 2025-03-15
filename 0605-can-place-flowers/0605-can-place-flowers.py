9a54a672ebfbb22b8a45b8163cb7b492ff8d4fab
Siyang Ye <50527023+siyangye@users.noreply.github.com>
2025-03-14 17:00:22 -0700

    Time: 11 ms (38%), Space: 18.5 MB (14.61%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,25 +1,25 @@
-#我想的办法：在flowerbed最前面/最后加0, 计算整个数组的1之间有多少0,当连续有3个0的时候，可以添加一个1。
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        bed = [0] + flowerbed + [0]
-        
         count = 0
-        consecutive_zeros = 0
-        
-        for plot in bed:
-            if plot == 0:
-                consecutive_zeros += 1
-            else:  # plot == 1
-                # Calculate how many flowers we can plant in this segment
-                if consecutive_zeros > 2:
-                    # We can place (z-1)//2 flowers in z consecutive zeros between flowers
-                    count += (consecutive_zeros - 1) // 2
+        m = len(flowerbed)
+        for i in range(m):
+            if i - 1 < 0 and i + 1< m:
+                if flowerbed[i + 1] == 0 and flowerbed[i] == 0:
+                    flowerbed[i] = 1
+                    count += 1
+            elif i + 1 >= m and i - 1>= 0:
+                if flowerbed[i - 1] == 0 and flowerbed[i] == 0:
+                    flowerbed[i] = 1
+                    count += 1
                     print(count)
-                consecutive_zeros = 0
-                
-        # Add the last segment if it ends with zeros
-        if consecutive_zeros > 2:
-            count += (consecutive_zeros -1) // 2
-            
+            elif i + 1 < m and i - 1>= 0:
+                if flowerbed[i - 1]== 0 and flowerbed[i + 1] ==0 and flowerbed[i] == 0:
+                    flowerbed[i] = 1
+                    count+= 1
+            elif i+ 1 == m and i - 1 == -1 and flowerbed[i]== 0:
+                count += 1
+        print(count)
+        print(n)
         return count >= n
+
         
